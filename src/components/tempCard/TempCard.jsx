f87101a0a146276ab9c832bac7d1f9f709c137f6
WBDev_Vivek <gawalevivek@gmail.com>
2024-06-07 15:49:19 +0530

    errorremoved

--- src/components/tempCard/TempCard.jsx
+++ src/components/tempCard/TempCard.jsx
@@ -3,7 +3,6 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { colors } from '@mui/material';
 
 export default function TempCard({ cityNameWeatherData }) {
     return (
